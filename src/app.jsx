be21b72651541402c24287f001272762bc09a220
thienn <thien.oslo@gmail.com>
2018-10-29 18:25:31 +0100

    Setup of initial project architecture
    + Setup of React with the base requirements for the exam. Babel, webpack. React & react-dom / router.
    + Setup of scripts. Mainly the dev one for now.
    + Upgraded the babel to the newest 7.0. - That will handle the transformation of code to browser friendly JS
    + Other personal dev tools like cheap-module-eval-source-map for error messages
    ---
    Should be only necessary to do npm install & npm run dev-server to start
    ---
    Next:
    Setup the router
    Will setup Express, websocket & passport dependenices when it will be used.

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = (
+   <div>
+    Test if it works
+    <p>
+        Checking if --watch works fine
+    </p>
+   </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// entry -> output
+
+const path = require('path');
+
+// https://webpack.js.org/concepts/#output
+
+// module.exports is a node feature to expose object to another file. So webpack will grab this file
+// run it and access to whatever is defined here
+// https://webpack.js.org/
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    // loader let you customize the behavior of webpack
+    // in this case, what to do with a JSX file
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.jsx$/,
+            exclude: /node_modules/
+        }]
+    },
+    // https://webpack.js.org/configuration/devtool/ on how to use devtool
+    /*
+        for this case: cheap-module-eval-source-map - original source (lines only)
+    */
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+    }
+};
+
+// __dirname will give the absolute path to your project, then it can specify the public folder etc
+
+
